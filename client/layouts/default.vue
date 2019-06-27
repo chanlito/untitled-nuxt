1e4c588e6051d42d6a7d01403b677853d52cf656
Chanlito Hem <chanlitohem@gmail.com>
2019-06-28 00:52:11 +0700

    refactor: :lipstick: adjust layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -2,19 +2,23 @@
   <v-app>
     <!-- Application Top Bar -->
     <v-app-bar app clipped-left color="primary" dark>
-      <v-app-bar-nav-icon @click="toggleDrawer" />
+      <v-app-bar-nav-icon class="hidden-lg-and-up" @click="toggleDrawer" />
       <v-toolbar-title class="title" v-text="appBar.title" />
       <v-spacer />
       <vv-app-bar-menu />
     </v-app-bar>
 
     <v-content>
-      <v-container>
-        <nuxt />
+      <v-container fluid>
+        <v-layout>
+          <v-flex xs12 sm12 md12 lg8 offset-lg2 xl8 offset-xl2>
+            <nuxt />
+          </v-flex>
+        </v-layout>
       </v-container>
     </v-content>
 
-    <v-navigation-drawer v-model="drawer" app clipped>
+    <v-navigation-drawer v-model="drawer" app clipped :width="280">
       <vv-navigation-drawer-list />
     </v-navigation-drawer>
   </v-app>
@@ -38,4 +42,11 @@ export default class DefaultLayout extends mixins(AppBarMixin) {
 .v-toolbar__content
   +screen(md-and-up)
     padding: 0 24px
+</style>
+
+<style lang="sass" scoped>
+.v-toolbar__title
+  &.title
+    +screen(lg-and-up)
+      padding-left: 0 !important
 </style>
